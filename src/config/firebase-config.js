fc5b326218afa9f11cf0d9233ed1043069d1359e
dennismuturi <dennismuturi1@gmail.com>
2022-10-10 09:43:59 +0300

    edited firebase config

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -9,11 +9,11 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "services-bbe0f.firebaseapp.com",
-  projectId: "services-bbe0f",
-  storageBucket: "services-bbe0f.appspot.com",
-  messagingSenderId: "251479731219",
-  appId: "1:251479731219:web:e3e6190b156ddd75f063e6"
+  authDomain: "backend-84468.firebaseapp.com",
+  projectId: "backend-84468",
+  storageBucket: "backend-84468.appspot.com",
+  messagingSenderId: "1061084680742",
+  appId: "1:1061084680742:web:816d3e2497b3dc37ebcce7"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
